d24c679214f669e4f313436228a58a80d9935534
kavin2483 <148341188+kavin2483@users.noreply.github.com>
2023-10-26 12:20:31 +0530

    Add files via upload

---
+++ DoWhileDemo.java
@@ -0,0 +1,20 @@
+package statements;
+
+public class DoWhileDemo
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Loop Starts");
+		int i=11;
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<=15);
+		System.out.println("Loop ends");
+
+	}
+
+}
