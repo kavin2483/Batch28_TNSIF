7e5dcca0f949e42f047d6bc540118b9b85dc9307
kavin2483 <148341188+kavin2483@users.noreply.github.com>
2023-10-31 13:00:20 +0530

    Add files via upload

---
+++ EncapslationDemo.java
@@ -0,0 +1,14 @@
+package oops_encapslation;
+
+public class EncapslationDemo {
+
+	public static void main(String[] args) 
+	{
+	OopsConceptDemo obj=new OopsConceptDemo();
+	obj.setSerialNum(101);
+	obj.setName("XYZ");
+	obj.setAge(20);
+	System.out.println(obj);
+	}
+
+}
